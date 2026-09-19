4dba7843c1b060a07c906031f0111104540c1366
Akshaya1827 <akshayapatel2727@gmail.com>
2026-09-19 11:43:22 +0530

    Implement authentication and role authorization

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const dns = require("dns");
-
+const authRoutes = require("./routes/authRoutes");
 dns.setServers(["8.8.8.8"]);
 dotenv.config();
 
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
     res.json({
         message: "ClubOps AI backend is running"
