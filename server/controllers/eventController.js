ddb74db9f6dab86f4e4d1682e54a29cc6e4422be
pritikashah <shahpritika19@gmail.com>
2026-09-19 11:35:19 +0530

    Add event controller

---
+++ server/controllers/eventController.js
@@ -0,0 +1,31 @@
+const Event = require("../models/Event");
+
+// Create a new event
+const createEvent = async (req, res) => {
+  try {
+    const { title, description, date, location } = req.body;
+
+    const event = await Event.create({
+      title,
+      description,
+      date,
+      location,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Event created successfully",
+      event,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to create event",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createEvent,
+};
